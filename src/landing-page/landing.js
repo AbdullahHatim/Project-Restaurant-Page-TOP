c60d8c2eb9b084c722417a1e5098d96aaec93e93
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-03-07 10:22:48 -0800

    Fix: Improve Home Page Styling

--- src/landing-page/landing.js
+++ src/landing-page/landing.js
@@ -1,6 +1,14 @@
 import image from "./background.jpg";
 
 export default function Home() {
+  content.innerHTML = "";
+  const quote = document.createElement("div");
+  quote.classList.add("quote")
+  quote.innerHTML = 
+  `<h1>Welcome To J's Pizza</h1>
+  <p>Baking The Best Pizza since 2004</p>
+  `;
+  content.appendChild(quote);
   content.style.background = "";
   content.style.backgroundImage = `url(${image})`;
   content.style.backgroundRepeat = "no-repeat";
